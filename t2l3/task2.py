d00689389dc3302bc948a2af995f55b0c836d001
try2wonder <ilja.jan@yandex.ru>
2024-11-05 16:45:49 +0300

    yeah

---
+++ t2l3/task2.py
@@ -0,0 +1,13 @@
+# Австралопитек 
+# Человек умелый 
+# Человек прямоходящий 
+# Неандерталец  
+# Человек разумный 
+
+print("Enter the stages")
+s1 = input("Stage1: ")
+s2 = input("Stage2: ")
+s3 = input("Stage3: ")
+s4 = input("Stage4: ")
+s5 = input("Stage5: ")
+print(s1,s2,s3,s4,s5, sep="=>")
